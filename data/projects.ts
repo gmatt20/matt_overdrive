9e05ace66905c2bc5fe58b12113f7264a536f43e
Matthew Garcia <gmatthew2765@gmail.com>
2026-01-23 20:12:07 -0800

    cleaned up metadata

---
+++ data/projects.ts
@@ -0,0 +1,32 @@
+import { StaticImageData } from "next/image";
+import tkd from "@/public/tkd.webp";
+import acm from "@/public/acm.webp";
+import aisc from "@/public/aisc.webp";
+
+interface Project {
+  url: StaticImageData;
+  link: string;
+  title: string;
+  alt: string;
+}
+
+export const ProjectData: Project[] = [
+  {
+    url: tkd,
+    link: "https://tkd.ucrhighlanders.org/",
+    title: "TKD @ UCR Website",
+    alt: "Picture of The Taekwondo @ UCR Website",
+  },
+  {
+    url: acm,
+    link: "https://acm.cs.ucr.edu/",
+    title: "ACM @ UCR",
+    alt: "Picture of the ACM @ UCR Website",
+  },
+  {
+    url: aisc,
+    link: "https://aisc.ucrhighlanders.org/",
+    title: "AI Student Collective @ UCR",
+    alt: "Picture of the AI Student Collective @ UCR Website",
+  },
+];
